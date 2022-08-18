a3b54dd3275a661680368206ee6c5edcc6f46ac3
Marek Gierlinski <mgierlinski@dundee.ac.uk>
2022-08-18 08:36:56 +0100

    KEGG queries are now in batches

--- R/kegg.R
+++ R/kegg.R
@@ -10,6 +10,8 @@
 #'   full list of available KEGG species can be found by using
 #'   \code{KEGGREST::keggList("organism")}. The column \code{organism} contains
 #'   the codes used here.
+#' @param batch_size Nubmer of pathways sent to KEGG database in one query. The
+#'   maximum allowed is 10.
 #'
 #' @return A list with \code{terms} and \code{mapping} tibbles.
 #' @export
@@ -18,25 +20,29 @@
 #' \dontrun{
 #' kegg_data <- fetch_kegg("hsa")
 #' }
-fetch_kegg <- function(species) {
+fetch_kegg <- function(species, batch_size = 10) {
   lst <- KEGGREST::keggList("pathway", species)
   terms <- tibble::tibble(
     term_id = names(lst) |> stringr::str_remove("path:"),
     term_name = lst
   )
+  pids <- terms$term_id
+  batches <- split(pids, ceiling(seq_along(pids) / batch_size))
 
-  pb <- progress::progress_bar$new(total = nrow(terms))
-  mapping <- purrr::map_dfr(terms$term_id, function(path_id) {
-    pw <- KEGGREST::keggGet(path_id)
+  pb <- progress::progress_bar$new(total = length(batches))
+  mapping <- purrr::map_dfr(batches, function(batch) {
+    pws <- KEGGREST::keggGet(batch)
     pb$tick()
-    if (!is.null(pw[[1]]$GENE)) {
-      # KEGG list of genes is a vector with alternate ID and gene description
-      gns <-  pw[[1]]$GENE
-      ids <- gns[seq(1, length(gns) - 1, 2)]
-      genes <- gns[seq(2, length(gns), 2)] |>
-        stringr::str_remove(";.*$")  # attempt to extract gene name
-      tibble::tibble(gene_id = ids, gene_symbol = genes, term_id = path_id)
-    }
+    purrr::map_dfr(pws, function(pw) {
+      if (!is.null(pw$GENE)) {
+        # KEGG list of genes is a vector with alternate ID and gene description
+        gns <-  pw$GENE
+        ids <- gns[seq(1, length(gns) - 1, 2)]
+        genes <- gns[seq(2, length(gns), 2)] |>
+          stringr::str_remove(";.*$")  # attempt to extract gene name
+        tibble::tibble(gene_id = ids, gene_symbol = genes, term_id = path_id)
+      }
+    })
   })
 
   list(
