a7eb196902cad75bb19a752bb1bc0675806c00b8
Marek Gierlinski <mgierlinski@dundee.ac.uk>
2022-08-22 09:33:09 +0100

    Required for no lazy data

--- R/data.R
+++ R/data.R
@@ -3,6 +3,7 @@
 #' A set of gene names for proteins that could be detected in a typical
 #' proteomics experiment on human samples.
 #'
+#' @usage data(exmpl_all)
 #' @format A character vector with 7075 elements.
 "exmpl_all"
 
@@ -11,5 +12,6 @@
 #'
 #' A set of gene names manually selected to illustrate functional enrichment.
 #'
+#' @usage data(exmpl_sel)
 #' @format A character vector with 20 elements.
 "exmpl_sel"
