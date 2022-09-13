4caf1be177498696be73adb1d86f1b64f5e4ac36
Marek Gierlinski <mgierlinski@dundee.ac.uk>
2022-09-13 13:57:46 +0100

    Replacing KEGGREST with own simple API parsers.

--- R/kegg.R
+++ R/kegg.R
@@ -1,18 +1,110 @@
+#' A helper function to retrieve API data from KEGG
+#'
+#' @param path Path in the query
+#'
+#' @return A string with result
+fetch_kegg_data <- function(path) {
+  base_url <-  "https://rest.kegg.jp"
+  url <- file.path(base_url, path)
+  assert_url_path(url)
+  res <- httr::GET(url)
+  rawToChar(res$content)
+}
+
+
 #' Find all species available from KEGG
 #'
-#' @return A tibble, in which column \code{organism} contains species
+#' @return A tibble, in which column \code{designation} contains species
 #'   designations used in function \code{fetch_kegg}.
 #' @export
 #'
 #' @examples
 #' spe <- fetch_kegg_species()
 fetch_kegg_species <- function() {
-  # Binding variables from non-standard evaluation locally
-  organism <- NULL
+  s <- fetch_kegg_data("list/organism")
+  readr::read_tsv(I(s), col_names = c("id", "designation", "species", "phylogeny"), show_col_types = FALSE)
+}
 
-  KEGGREST::keggList("organism") |>
-    tibble::as_tibble() |>
-    dplyr::rename(designation = organism)
+
+#' Download pathway data from KEGG
+#'
+#' @param species A valid species designation used by KEGG.
+#'
+#' @return A tibble with columns \code{gene_id} and \code{term_id}.
+fetch_kegg_pathways <- function(species) {
+  # Binding variables from non-standard evaluation locally
+  term_id <- NULL
+
+  s <- fetch_kegg_data(stringr::str_glue("list/pathway/{species}"))
+  readr::read_tsv(I(s), col_names = c("term_id", "term_name"), show_col_types = FALSE) |>
+    dplyr::mutate(term_id = stringr::str_remove(term_id, "path:"))
+}
+
+
+#' Parse flat file returned from KEGG GET API
+#'
+#' @param s A string containing flat file result
+#'
+#' @import stringr
+#' @return A tibble with \code{gene_is}, \code{gene_symbol} and \code{term_id}.
+parse_kegg_genes <- function(s) {
+  # Binding variables from non-standard evaluation locally
+  data <- NULL
+
+  entries <- str_split(s, "///") |> unlist()
+  entries <- entries[1:(length(entries) - 1)] # there is /// at the end
+  purrr::map_dfr(entries, function(entry) {
+    d <- str_split(entry, "\n") |>
+      unlist()
+    n <- length(d)
+    i <- 1
+
+    # find ENTRY key
+    while(i <= n & !(str_detect(d[i], "^ENTRY")))
+      i <- i + 1
+    paths <- str_split(d[i], "\\s+") |> unlist()
+    pathway <- paths[2]
+
+    # find GENE key
+    i <- 1
+    while(i <= n & !(str_detect(d[i], "^GENE")))
+      i <- i + 1
+    # extract genes
+    genes <- str_remove(d[i], "GENE\\s+")
+    i <- i + 1
+    while(i <= n & str_detect(d[i], "^\\s+")) {
+      genes <- c(genes, str_remove(d[i], "^\\s+"))
+      i <- i + 1
+    }
+    genes |>
+      str_remove(";.+$") |>
+      tibble::as_tibble_col(column_name = "data") |>
+      tidyr::separate(data, c("gene_id", "gene_symbol"), sep = "\\s+") |>
+      tibble::add_column(term_id = pathway)
+  })
+}
+
+
+#' Download term - gene mapping from KEGG
+#'
+#' @param pathways A character vector with KEGG pathways
+#' @param batch_size Number of pathways sent to KEGG database in one query. The
+#'   maximum allowed is 10.
+#'
+#' @return A tibble with columns \code{gene_id} and \code{term_id}
+fetch_kegg_mapping <- function(pathways, batch_size) {
+  assertthat::is.string(pathways)
+  batches <- split(pathways, ceiling(seq_along(pathways) / batch_size))
+
+  pb <- progress::progress_bar$new(total = length(batches))
+  purrr::map_dfr(batches, function(batch) {
+    dbentries <- paste(batch, collapse = "+")
+
+    # this returns a flat file
+    s <- fetch_kegg_data(stringr::str_glue("get/{dbentries}"))
+    pb$tick()
+    parse_kegg_genes(s)
+  })
 }
 
 
@@ -36,46 +128,13 @@ fetch_kegg_species <- function() {
 #' @examples
 #' kegg_data <- fetch_kegg("sce")
 fetch_kegg <- function(species, batch_size = 10) {
-  # Binding variables from non-standard evaluation locally
-  path_id <- NULL
-
   assert_that(!missing(species), msg = "Argument 'species' is missing.")
   assert_species(species, fetch_kegg_species)
   assert_that(is.count(batch_size))
   assert_that(batch_size <= 10, msg = "batch_size needs to be between 1 and 10")
 
-  lst <- tryCatch(
-    KEGGREST::keggList("pathway", species),
-    error = function(err)
-      stop(stringr::str_glue("There is a problem retrieving KEGG pathways for species '{species}'."))
-  )
-
-  terms <- tibble::tibble(
-    term_id = names(lst) |> stringr::str_remove("path:"),
-    term_name = lst
-  )
-  pids <- terms$term_id
-  batches <- split(pids, ceiling(seq_along(pids) / batch_size))
-
-  pb <- progress::progress_bar$new(total = length(batches))
-  mapping <- purrr::map_dfr(batches, function(batch) {
-    pws <- tryCatch(
-      KEGGREST::keggGet(batch),
-      error = function(err)
-        stop(stringr::str_glue("There is a problem retrieving KEGG batch':\n{err}"))
-    )
-    pb$tick()
-    purrr::map_dfr(pws, function(pw) {
-      if (!is.null(pw$GENE)) {
-        # KEGG list of genes is a vector with alternate ID and gene description
-        gns <-  pw$GENE
-        ids <- gns[seq(1, length(gns) - 1, 2)]
-        genes <- gns[seq(2, length(gns), 2)] |>
-          stringr::str_remove(";.*$")  # attempt to extract gene name
-        tibble::tibble(gene_id = ids, gene_symbol = genes, term_id = unname(pw$ENTRY))
-      }
-    })
-  })
+  terms <- fetch_kegg_pathways(species)
+  mapping <- fetch_kegg_mapping(terms$term_id, batch_size)
 
   list(
     terms = terms,
