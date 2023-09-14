87ed8ca93d62900414b420a7312da4a48d6ad619
MarekGierlinski <Marek.Gierlinski@gmail.com>
2023-09-14 12:03:50 +0100

    Adding package-level documentation.

---
+++ R/fenr-package.R
@@ -0,0 +1,10 @@
+#' @description
+#'
+#' This R package provides a fast and efficient method for functional enrichment
+#' analysis, optimized for speed and designed for use in interactive
+#' applications, such as *Shiny* apps.
+#'
+#' To learn more about fenr, start with the vignette: vignette("fenr").
+#'
+#' @keywords internal
+"_PACKAGE"
